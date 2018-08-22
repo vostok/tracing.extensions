440cddd8e80b01377d422757e8b0c91e2ff7a2f9
n.kochnev <n.kochnev@skbkontur.ru>
2018-08-22 17:13:11 +0500

    added test extensions for span kinds

---
+++ Vostok.Tracing.Extensions/Abstractions/IHttpRequestClusterSpanBuilder.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Vostok.Tracing.Abstractions;
+
+namespace Vostok.Tracing.Extensions.Abstractions
+{
+    public interface IHttpRequestClusterSpanBuilder : IHttpRequestSpanBuilder
+    {
+        void SetClusterDetails(string requestStrategy, string status);
+    }
+}
---
+++ Vostok.Tracing.Extensions/HttpRequest_Extensions.cs
@@ -0,0 +1,36 @@
+﻿using Vostok.Tracing.Abstractions;
+using Vostok.Tracing.Extensions.Abstractions;
+using Vostok.Tracing.Extensions.SpanBuilders;
+
+namespace Vostok.Tracing.Extensions
+{
+    public static class HttpRequestExtensions
+    {
+        public static IHttpRequestSpanBuilder BeginHttpRequestClientSpan(this ITracer tracer)
+        {
+            var spanBuilder = tracer.BeginSpan();
+
+            spanBuilder.SetAnnotation(AnnotationNames.SpanKind, "http-request-client");
+
+            return new HttpRequestSpanBuilder(spanBuilder);
+        }
+
+        public static IHttpRequestServerSpanBuilder BeginHttpRequestServerSpan(this ITracer tracer)
+        {
+            var spanBuilder = tracer.BeginSpan();
+
+            spanBuilder.SetAnnotation(AnnotationNames.SpanKind, "http-request-server");
+
+            return new HttpRequestServerSpanBuilder(spanBuilder);
+        }
+
+        public static IHttpRequestClusterSpanBuilder BeginHttpRequestClusterSpan(this ITracer tracer)
+        {
+            var spanBuilder = tracer.BeginSpan();
+
+            spanBuilder.SetAnnotation(AnnotationNames.SpanKind, "http-request-cluster");
+
+            return new HttpRequestSpanClusterBuilder(spanBuilder);
+        }
+    }
+}
