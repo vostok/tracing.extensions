7f3cc55e5b4bc3ab3bacedc945727163c1700310
n.kochnev <n.kochnev@skbkontur.ru>
2018-08-22 17:21:20 +0500

    use InternalSpanBuilder

--- Vostok.Tracing.Extensions/SpanBuilders/HttpRequestServerSpanBuilder.cs
+++ Vostok.Tracing.Extensions/SpanBuilders/HttpRequestServerSpanBuilder.cs
@@ -1,9 +1,10 @@
-﻿using Vostok.Tracing.Abstractions;
+﻿using System;
+using Vostok.Tracing.Abstractions;
 using Vostok.Tracing.Extensions.Abstractions;
 
 namespace Vostok.Tracing.Extensions.SpanBuilders
 {
-    internal class HttpRequestServerSpanBuilder : HttpRequestSpanBuilder, IHttpRequestServerSpanBuilder
+    internal class HttpRequestServerSpanBuilder : InternalSpanBuilder, IHttpRequestServerSpanBuilder
     {
         private readonly ISpanBuilder spanBuilder;
 
@@ -16,6 +17,22 @@ namespace Vostok.Tracing.Extensions.SpanBuilders
         public void SetClientDetails(string name, string address)
         {
             spanBuilder.SetAnnotation(AnnotationNames.Http.Client.Name, name);
+            spanBuilder.SetAnnotation(AnnotationNames.Http.Client.Address, address);
+        }
+
+        public void SetRequestDetails(Uri uri, string httpMethodName, int contentLength)
+        {
+            spanBuilder.SetAnnotation(AnnotationNames.Http.Request.Url, uri.ToString());
+            spanBuilder.SetAnnotation(AnnotationNames.Http.Request.Method, httpMethodName);
+            spanBuilder.SetAnnotation(AnnotationNames.Http.Request.ContentLength, contentLength.ToString());
+
+            spanBuilder.SetAnnotation(AnnotationNames.Operation, $"({httpMethodName.ToUpper()}): {uri}");
+        }
+
+        public void SetResponseDetails(int responseCode, int contentLength)
+        {
+            spanBuilder.SetAnnotation(AnnotationNames.Http.Response.StatusCode, responseCode.ToString());
+            spanBuilder.SetAnnotation(AnnotationNames.Http.Response.ContentLength, contentLength.ToString());
         }
     }
 }
---
+++ Vostok.Tracing.Extensions/SpanBuilders/InternalSpanBuilder.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Vostok.Tracing.Abstractions;
+
+namespace Vostok.Tracing.Extensions.SpanBuilders
+{
+    internal class InternalSpanBuilder : ISpanBuilder
+    {
+        protected readonly ISpanBuilder spanBuilder;
+
+        public InternalSpanBuilder(ISpanBuilder spanBuilder)
+        {
+            this.spanBuilder = spanBuilder;
+        }
+
+        public bool IsEndless
+        {
+            get => spanBuilder.IsEndless;
+            set => spanBuilder.IsEndless = value;
+        }
+
+        public void Dispose()
+        {
+            spanBuilder.Dispose();
+        }
+
+        public void SetAnnotation(string key, string value, bool allowOverwrite = true)
+        {
+            spanBuilder.SetAnnotation(key, value, allowOverwrite);
+        }
+
+        public void SetBeginTimestamp(DateTimeOffset timestamp)
+        {
+            spanBuilder.SetBeginTimestamp(timestamp);
+        }
+
+        public void SetEndTimestamp(DateTimeOffset timestamp)
+        {
+            spanBuilder.SetEndTimestamp(timestamp);
+        }
+    }
+}
