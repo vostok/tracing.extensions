620af219477608223a2459f431ec196f845f96fe
kungurtsev <kungurtsev@skbkontur.ru>
2019-12-23 14:38:22 +0500

    Revert "Revert "SetRequestDetails with string url""
    This reverts commit f89b9e09b5a21a27710b67525bb719f33215dd69.

--- Vostok.Tracing.Extensions/Http/IHttpRequestServerSpanBuilder.cs
+++ Vostok.Tracing.Extensions/Http/IHttpRequestServerSpanBuilder.cs
@@ -6,7 +6,7 @@ namespace Vostok.Tracing.Extensions.Http
 {
     /// <summary>
     /// <para>Represents a builder that produces annotations relevant for HTTP request spans of <see cref="WellKnownSpanKinds.HttpRequest.Server"/> kind.</para>
-    /// <para>See <see cref="IHttpRequestSpanBuilder.SetRequestDetails"/>, <see cref="IHttpRequestSpanBuilder.SetResponseDetails"/> and <see cref="SetClientDetails"/> for more info.</para>
+    /// <para>See <see cref="IHttpRequestSpanBuilder.SetRequestDetails(System.Uri,string,System.Nullable{long})"/>, <see cref="IHttpRequestSpanBuilder.SetResponseDetails"/> and <see cref="SetClientDetails"/> for more info.</para>
     /// </summary>
     [PublicAPI]
     public interface IHttpRequestServerSpanBuilder : IHttpRequestSpanBuilder
